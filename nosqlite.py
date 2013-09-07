75f3eb9489862c301202c128919935c28a47febf
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2013-09-07 03:53:54 +0200

    Added IF NOT EXISTS to table creation
    I sometimes ran into the problem that the server wanted to create an already existing table. This is the simplest fix for this problem for now and should not cause any harm.

--- nosqlite.py
+++ nosqlite.py
@@ -763,7 +763,7 @@ class Collection(object):
             ['a', 'b', 'c']
         """
         self._validate_column_names(columns)
-        self.database('CREATE TABLE "%s" (%s)'%(self.name, ', '.join('"%s"'%s for s in columns)))
+        self.database('CREATE TABLE IF NOT EXISTS "%s" (%s)'%(self.name, ', '.join('"%s"'%s for s in columns)))
         
     ###############################################################
     # Inserting documents: one at a time or in a batch
